d4c415f24b170f81e4148f9d149c0b6343442ea9
Afren12am <77343003+Afren12am@users.noreply.github.com>
2021-09-07 20:46:10 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+
+        public string FIO;
+        public int age;
+        public double weight;
+        public string colorEyes;
+        public Person()
+        {
+            FIO = "";
+            colorEyes = "";
+            age = 0;
+            weight = 0;
+        }
+        public Person(string FIO, int age, double weight, string colorEyes)
+        {
+            this.FIO = FIO;
+            this.colorEyes = colorEyes;
+            this.age = age;
+            this.weight = weight;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine("Имя человека:" + FIO + "\nВозраст:" + age + "\nВес: " + weight
+                              + "\nЦвет глаз: " + colorEyes);
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person artem = new Person("Lukmanov Artem Ildarovich", 18, 67, "Green");
+
+            artem.Print();
+
+        }
+    }
+}
